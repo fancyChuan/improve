4f8f8a73f88fe2d9108801ff3ad6c4d4bcad43cf
tn-fancy <1247375074@qq.com>
2019-11-29 18:01:10 +0800

    sqlparse的使用&补充

--- python元编程/astParse/sqlparseLearn.py
+++ python元编程/astParse/sqlparseLearn.py
@@ -12,6 +12,15 @@ query = "select id as myid ,r.fname, r.lname as mylname, address, max(a.total_de
          from res_users as r left join res_partner as p on p.id=r.partner_id \
          where name = (select name from res_partner where id = 1)"
 
+query = """update tb_compensate_detail_tmp a        
+           set a.curDueMoney = case when a.loanType = '先息后本' then a.dueMonths*a.curShouldInterest 
+           else a.dueMonths*(a.curShouldInterest+a.curShouldCapital) end,
+          a.ReturnStatus = a.dueLevel  
+          where a.dueDays>0 and a.statdate = '20191101'"""
+# query = "select datacode from tb_dist_rpt where datatype = 'ContractTemplateType' " \
+#         "and datacode in ('bzcd','bzcsd','ghzc','ghyc','sjshcd','bzmd','unifiedCarContractTemplate','unifiedCreditContractTemplate') " \
+#         "UNION ALL  SELECT 'car_owner_credit'"
+
 query_tokens = sqlparse.parse(query)[0].tokens
 for token in query_tokens:
     if token.ttype is None and isinstance(token, sql.IdentifierList):
@@ -25,3 +34,14 @@ for token in query_tokens:
             print("------\n" + item.value)
             print(item.get_parent_name(), item.get_name(), item.get_alias(), item.get_real_name())
 
+print("==============================================")
+# select a from x 只有一个字段的时候，类型并不是 IdentifierList 这里需要增强
+for token in query_tokens:
+    if token.value.lower() == "from":
+        break
+    elif token.ttype is None and isinstance(token, sql.IdentifierList):
+        for item in token.get_identifiers():
+            print("------\n" + item.value)
+            print(item.get_parent_name(), item.get_name(), item.get_alias(), item.get_real_name())
+    elif token.ttype is None and isinstance(token, sql.Identifier):
+        print(token.value)
